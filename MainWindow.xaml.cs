8fb1e54174d7b86902671c7c7129b2f897e5e0a3
KOKAB NAVEED <kokabnaveed2002@gmail.com>
2026-01-24 00:15:07 +0500

    Set MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,40 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using Project.Services;
 
 namespace Project
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            LoadAllDataByDefault();
+        }
+
+        // Load all users/data when the window opens
+        private void LoadAllDataByDefault()
+        {
+            using (var db = new AppDbContext())
+            {
+                // Load Email users as default
+                var users = db.EmailUsers
+                              .Select(u => new
+                              {
+                                  u.Id,
+                                  u.FirstName,
+                                  u.LastName,
+                                  u.EmailAddress,
+                                  u.Company,
+                                  u.StorageGB
+                                  // Do NOT include Password
+                              })
+                              .ToList();
+
+                dataGrid.ItemsSource = users;
+            }
+
+            dataGrid.Visibility = Visibility.Visible;
         }
 
         private void EmailMenu_Click(object sender, RoutedEventArgs e)
@@ -18,10 +42,9 @@ namespace Project
             Email emailUser = new Email();
             emailUser.Show();
             this.Close();
-
         }
 
-        private void DomainMenu_Click(Object sender, RoutedEventArgs e)
+        private void DomainMenu_Click(object sender, RoutedEventArgs e)
         {
             Domain domainUser = new Domain();
             domainUser.Show();
@@ -40,5 +63,78 @@ namespace Project
             this.Close();
         }
 
+        // Show Software Data
+        private void SoftwareData_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new AppDbContext())
+            {
+                var softwares = db.Softwares
+                                  .Select(s => new
+                                  {
+                                      s.Id,
+                                      s.SoftwareName,
+                                      s.PlanType,
+                                      s.Email,
+                                      s.Amount,
+                                      s.SubscribedDate,
+                                      s.RenewalDate
+                                  })
+                                  .ToList();
+
+                dataGrid.ItemsSource = softwares;
+            }
+
+            dataGrid.Visibility = Visibility.Visible;
+        }
+
+        // Show Email Data
+        private void EmailData_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new AppDbContext())
+            {
+                var users = db.EmailUsers
+                              .Select(u => new
+                              {
+                                  u.Id,
+                                  u.FirstName,
+                                  u.LastName,
+                                  u.EmailAddress,
+                                  u.Company,
+                                  u.StorageGB
+                                  // Password excluded
+                              })
+                              .ToList();
+
+                dataGrid.ItemsSource = users;
+            }
+
+            dataGrid.Visibility = Visibility.Visible;
+        }
+
+        // Show Domain Data
+        private void DomainData_Click(object sender, RoutedEventArgs e)
+        {
+            using (var db = new AppDbContext())
+            {
+                var domains = db.Domains
+                                .Select(d => new
+                                {
+                                    d.Id,
+                                    d.DomainName,
+                                    d.RegisteredDate,
+                                    d.RenewalDate,
+                                    d.Registrar,
+                                    d.Amount,
+                                    d.NameServer1,
+                                    d.NameServer2,
+                                    d.AutoRenew
+                                })
+                                .ToList();
+
+                dataGrid.ItemsSource = domains;
+            }
+
+            dataGrid.Visibility = Visibility.Visible;
+        }
     }
 }
